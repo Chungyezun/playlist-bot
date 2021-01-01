18321deffa9884b8fc4811d5e672b416076b6622
Chungyezun <maple0729@kaist.ac.kr>
2021-01-01 23:04:31 +0900

    add ralo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const client = new Discord.Client();
 
 client.once("ready", () => {
@@ -9,6 +11,11 @@ client.on("message", (message) => {
   console.log(message.content);
   if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send(client.ws.ping + " ms");
+  }
+  if (message.content.startsWith(config.prefix + "랄로해봐")) {
+    var path = "./ralo.jpg";
+    const attachment = new Attachment(path);
+    msg.channel.send(attachment);
   }
 });
 
