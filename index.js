a0655c49e8700b43c3746a60483bc6ddd7619edb
Chungyezun <maple0729@kaist.ac.kr>
2021-01-02 00:06:29 +0900

    ralo final

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// const config = require("./config.json");
-const { Client, Attachment } = require("discord.js");
+const config = require("./config.json");
+const Discord = require("discord.js");
 // const axios = require("axios");
 // const cheerio = require("cheerio");
-const client = new Client();
+const client = new Discord.Client();
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on("message", (message) => {
   console.log(message.content);
-  //   if (message.content.startsWith(config.prefix + "ping")) {
-  if (message.content.startsWith("!ping")) {
+  if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send(client.ws.ping + " ms");
   }
-  if (message.conten.startsWith("!랄로해봐")) {
-    const attachment = new Attachment(
-      "https://s3.orbi.kr/data/file/united2/ddfbb85604c94b198f580eb21bf27283.jpeg"
-    );
-    message.channel.send(attachment);
+  if (message.content.startsWith("!랄로해봐")) {
+    message.channel.send({ files: ["./ralo.jpg"] });
   }
 });
-
+// client.login(config.token);
 client.login(process.env.TOKEN);
