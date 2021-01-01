1baae118a3d1e4e2acc33be99ad8792473e953e9
Chungyezun <maple0729@kaist.ac.kr>
2021-01-01 23:51:20 +0900

    1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!ping")) {
     message.channel.send(client.ws.ping + " ms");
   }
-  if (message.content === "!랄로해봐") {
+  if (message.conten.startsWith("!랄로해봐")) {
     const attachment = new Attachment(
       "https://s3.orbi.kr/data/file/united2/ddfbb85604c94b198f580eb21bf27283.jpeg"
     );
