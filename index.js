7335cb117a0f39fb362de2a601f5d29b076d5769
Chungyezun <maple0729@kaist.ac.kr>
2021-01-01 23:14:45 +0900

    uncomment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const config = require("./config.json");
+const config = require("./config.json");
 const Discord = require("discord.js");
 // const axios = require("axios");
 // const cheerio = require("cheerio");
@@ -9,8 +9,7 @@ client.once("ready", () => {
 });
 client.on("message", (message) => {
   console.log(message.content);
-  //   if (message.content.startsWith(config.prefix + "ping")) {
-  if (message.content.startsWith("!ping")) {
+  if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send(client.ws.ping + " ms");
   }
   //   if (message.content.startsWith(config.prefix + "랄로해봐")) {
